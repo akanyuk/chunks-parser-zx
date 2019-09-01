59bf2fbbc240cbd408cc1829f7c64424a933fbfd
nyuk <aka.nyuk@gmail.com>
2019-09-02 00:55:57 +0300

    removed keyframe support

--- tests/make.cmd
+++ tests/make.cmd
@@ -2,8 +2,7 @@
 
 del /q test*.sna
 
-rem sjasmplus.exe sources\test-1s.asm
-rem sjasmplus.exe sources\test-1s-attr.asm
-rem sjasmplus.exe sources\test-2s.asm
-rem sjasmplus.exe sources\test-2s-attr.asm
-sjasmplus.exe sources\test-2s-attr-keyframe.asm
+sjasmplus.exe sources\test-1s.asm
+sjasmplus.exe sources\test-1s-attr.asm
+sjasmplus.exe sources\test-2s.asm
+sjasmplus.exe sources\test-2s-attr.asm
